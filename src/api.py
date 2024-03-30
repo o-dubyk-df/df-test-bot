31158c8d82bdc9e5133e3f94af6a782814200fbe
Artur Boyun <arturboyun@gmail.com>
2024-03-30 19:45:49 +0200

    refactor: minimal data for telegram

--- src/api.py
+++ src/api.py
@@ -11,14 +11,31 @@ SQS_QUEUE_URL = os.getenv("SQS_QUEUE_URL", "")
 sqs = boto3.resource("sqs")
 queue = sqs.Queue(SQS_QUEUE_URL)
 
+{
+    "update_id": 813352806,
+    "message": {
+        "message_id": 186,
+        "chat": {"id": 123, "type": "private"},
+        "date": 1708367401,
+        "text": "11122 2hhheeellooo3332 3222",
+    },
+}
+
 
 def lambda_handler(event, context):
     try:
         event = json.loads(event["body"])
-        del event["message"]["from"]
-        event["message"]["chat"] = {
-            "id": event["message"]["chat"]["id"],
-            "type": event["message"]["chat"]["type"],
+        event = {
+            "update_id": event["update_id"],
+            "message": {
+                "message_id": event["message"]["message_id"],
+                "chat": {
+                    "id": event["message"]["chat"]["id"],
+                    "type": event["message"]["chat"]["type"],
+                },
+                "date": event["message"]["date"],
+                "text": event["message"]["text"],
+            },
         }
         queue.send_message(MessageBody=json.dumps(event))
         return {"statusCode": 200, "body": "Success"}
